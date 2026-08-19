30afd6dcf70493db333cc71be657e773e14d9607
nie-athishay <2025cs_athishayk_b@nie.ac.in>
2026-08-19 15:37:27 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,19 @@
+// class Demo{
+//     public static void main(String args[]){
+//         System.out.println("Hello, World!");
+//     }
+// }
+
+// class Demo{
+//     public static void main(String args[]){
+//         String name = "Akash";
+//         System.out.println("Name: "+ name);
+//         int age = 19;
+//         System.out.println("Age: "+ age);
+//         double marks = 70.0;
+//         System.out.println("Marks: "+ marks);
+//     }
+// }
+
+
+
