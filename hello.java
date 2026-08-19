97de2bfe02f443328534cbf9304a9b201dbbbf80
nie-athishay <2025cs_athishayk_b@nie.ac.in>
2026-08-19 16:23:53 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -16,4 +16,65 @@
 // }
 
 
+// class Demo{
+//     public static void main(String args[]){
+//         int a = 10;
+//         int b = 7;
+//         System.out.println("a: "+a+" b: "+b);
+//         System.out.println("a+b: "+ (a+b));
+//         System.out.println("a-b: "+ (a-b));
+//         System.out.println("a*b: "+ a*b);
+//         System.out.println("a/b: "+ a/b);
+//         System.out.println("a%b: "+ a%b);
+//     }
+// }
+
+// class Demo{
+//     public static void main(String[] args) {
+//         int p = 1000;
+//         int t = 5;
+//         double r = 0.3;
+//         double si = (p*t*r)/100;
+//         System.out.println("Principle: "+p+" Time: "+t+" Rate Of Interest : "+r);
+//         System.out.println("Simple Interest: "+si);
+//     }
+// }
+
+// import java.util.Scanner;
+// class Demo{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Number 1: ");
+//         int num1 = sc.nextInt();
+//         int num2 = sc.nextInt();
+//         System.out.println(num1+" + "+num2+" = "+(num1+num2));
+
+//     }
+// }
+
+import java.util.Scanner;
+// class Arrays{
+//     public static void main(String[] args) {
+//         int arr[][] =  new int[2][3];
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Row: ");
+//         int m = sc.nextInt();
+//         System.out.println("Column: ");
+//         int n = sc.nextInt();
+//         System.err.println("Enter Array Elements:");
+//         for (int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 arr[i][j] = sc.nextInt();
+//             }
+//         }
+//         for (int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 System.out.print(arr[i][j]+" ");
+//             }
+//             System.out.println("\n");
+//         }
+//     }
+// }
+
+
 
